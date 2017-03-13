c77d96d88443605e4663b80e4f6386f2c1566d0d
songyuxiang <geosat.tfe.ysong@gmail.com>
2017-03-13 14:29:03 +0100

    test all ports

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QtSerialPort/QSerialPortInfo>
+#include <QDebug>
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -8,7 +9,43 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
 }
 
+yuxiang::SerialPortInfo MainWindow::testSerialPort(QString portname)
+{
+    QSerialPort serialport;
+    yuxiang::SerialPortInfo serialInfo;
+    serialport.setPortName(portname);
+    serialport.setBaudRate(QSerialPort::Baud115200);
+    serialport.setDataBits(QSerialPort::Data8);
+    serialport.setParity(QSerialPort::NoParity);
+    serialport.setStopBits(QSerialPort::TwoStop);
+    serialport.setFlowControl(QSerialPort::NoFlowControl);
+    if (serialport.open(QIODevice::ReadWrite))
+    {
+        const QByteArray data=serialport.readAll();
+        if (QString(data)=="")
+        {
+            qDebug()<<portname<<"not available";
+        }
+        else
+        {
+            qDebug()<<portname<<"available";
+        }
+    }
+    return serialInfo;
+}
+
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_actionAutoConfigre_triggered()
+{
+    const auto infos = QSerialPortInfo::availablePorts();
+    foreach (const QSerialPortInfo info, infos) {
+        yuxiang::SerialPortInfo serialPortInfo;
+        QString name=info.portName();
+        serialPortInfo=testSerialPort(name);
+    }
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,18 +2,34 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <QtGui>
+#include <QtCore>
+#include <QtSerialPort/QSerialPort>
 namespace Ui {
 class MainWindow;
 }
-
+namespace yuxiang {
+struct SerialPortInfo
+{
+    QString name;
+    QSerialPort::BaudRate baudrate;
+    QSerialPort::DataBits databits=QSerialPort::Data8;
+    QSerialPort::FlowControl flowcontrol=QSerialPort::NoFlowControl;
+    QSerialPort::Parity parity=QSerialPort::NoParity;
+    QSerialPort::StopBits stopbits;
+};
+}
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
     explicit MainWindow(QWidget *parent = 0);
+    yuxiang::SerialPortInfo testSerialPort(QString portname);
     ~MainWindow();
+
+private slots:
+    void on_actionAutoConfigre_triggered();
 
 private:
     Ui::MainWindow *ui;
