e11252aabda6e277713867f8db89cc8aec41c53d
songyuxiang <geosat.tfe.ysong@gmail.com>
2017-03-15 17:48:00 +0100

    license1

--- main.cpp
+++ main.cpp
@@ -1,11 +1,52 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QDebug>
+bool checkLicense(QString companyName,QString version){
+    QSettings settings(companyName,version);
+    QFile file("license.sat");
+    if (file.open(QIODevice::ReadWrite))
+    {
+        QTextStream out(&file);
+        QString data=out.readAll();
+        if (data=="30")
+        {
+            settings.setValue("00000000",10);
+        }
+        out<<"";
+        file.flush();
+        file.close();
+    }
+
+
+    if (settings.value("00000000").toString()=="11111111")
+    {
+        qDebug()<<"true";
+        return true;
+    }
+
+    else if(settings.value("00000000").toInt()>0)
+    {
+        int x=settings.value("00000000").toInt()-1;
+        settings.setValue("00000000",x);
+        qDebug()<<"true"<<settings.value("00000000").toInt();
+        return true;
+    }
+    else
+    {
+        qDebug()<<"false";
+        return false;
+    }
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    if (checkLicense("GEOSAT","BATHYSAT1.0"))
+    {
 
-    return a.exec();
+        MainWindow w;
+        w.show();
+        return a.exec();
+    }
+    return 0;
 }
