c3c519972a2fa8e7e59b8f55c9462b63b4f2c6e6
songyuxiang <geosat.tfe.ysong@gmail.com>
2017-03-15 18:11:57 +0100

    license2

--- main.cpp
+++ main.cpp
@@ -1,26 +1,26 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QMessageBox>
 #include <QDebug>
 bool checkLicense(QString companyName,QString version){
     QSettings settings(companyName,version);
-    QFile file("license.sat");
-    if (file.open(QIODevice::ReadWrite))
-    {
-        QTextStream out(&file);
-        QString data=out.readAll();
-        if (data=="30")
-        {
-            settings.setValue("00000000",10);
-        }
-        out<<"";
-        file.flush();
-        file.close();
-    }
+//    QFile file("license.sat");
+//    if (file.open(QIODevice::ReadWrite))
+//    {
+//        QTextStream out(&file);
+//        QString data=out.readAll();
+//        if (data=="30")
+//        {
+//            settings.setValue("00000000",10);
+//        }
+//        out<<"";
+//        file.flush();
+//        file.close();
+//    }
 
 
     if (settings.value("00000000").toString()=="11111111")
     {
-        qDebug()<<"true";
         return true;
     }
 
@@ -28,12 +28,10 @@ bool checkLicense(QString companyName,QString version){
     {
         int x=settings.value("00000000").toInt()-1;
         settings.setValue("00000000",x);
-        qDebug()<<"true"<<settings.value("00000000").toInt();
         return true;
     }
     else
     {
-        qDebug()<<"false";
         return false;
     }
 }
@@ -41,6 +39,7 @@ bool checkLicense(QString companyName,QString version){
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    qDebug()<<"test";
     if (checkLicense("GEOSAT","BATHYSAT1.0"))
     {
 
